07f803fc9b97b63c3b9fef973253266dbd91c07c
Derian Molina Lopez <derianmolina220@gmail.com>
2023-11-26 11:05:53 -0700

    practica 3 terminada

--- practicash/practica3/tiposPersonas.hs
+++ practicash/practica3/tiposPersonas.hs
@@ -3,13 +3,14 @@
 {-
 NOMBRE,CARRERA,NACIMIENTO,MUERTE
 -}
-type Persona = (String,String,Int,Int)
+
 --MAS ABAJO ME PDIE DEFINIR UN TIPO NOMBRE
 type Nombre = String
 type Carrera = String
-type Edad = Int
+type Nacimiento = Int
 type Muerte = Int
 type Profesion = String
+type Persona = (Nombre,Profesion,Nacimiento,Muerte)
 -- base de datos
 personas :: [(String,String,Int,Int)]
 personas = [
@@ -23,8 +24,10 @@ personas = [
 
 main :: IO()
 main = do
+    --print (ocupacion personas "Pintura")
+    print(vivas personas 1750)
 
-    print (musicos personas)
+    {-******************************************-}
 -- funci�n que proporciona la lista de la nombres de personas de la BD
 nombres :: [(String,String,Int,Int)] -> [String]
 nombres [] = []--caso base, toco hacer recursividad
@@ -34,8 +37,6 @@ nombres ((nombre,_,_,_):xs) = nombre : nombres xs
 
 
 -- funci�n que retorna los nombres de las personas que son m�sicos
-
-
 esMusico :: Persona -> Bool
 esMusico (_, carrera, _, _) = carrera == "Musica"
 
@@ -50,6 +51,8 @@ ocupacion personas profesion = map (\(nombre, _, _, _) -> nombre) (filter (\(_, 
 -- Definir la funci�n vivas tal que (vivas bd a) es la lista de 
 -- los nombres de las personas de la base de datos bd que 
 -- estaban vivas en el a�o a
---vivas :: [(String,String,Int,Int)] -> Int -> [String]
+{-[COLLECCION DE PERSONA]---> AÑO--> [lista de personas vivas en ese año -}
+vivas :: [(String,String,Int,Int)] -> Int -> [String]
+vivas personas a = map (\(nombre, _, nacimiento, muerte) -> nombre) (filter (\(_, _, nacimiento, muerte) -> nacimiento <= a && muerte >= a) personas)
 
 
